b730a3d7a88aa7f3583c1e857957d9897d71256a
ClubPooks <64919987+ClubPooks@users.noreply.github.com>
2020-05-06 17:05:19 -0300

    Create postagem.php

---
+++ postagem.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Club Pooks</title>
+	<link rel="stylesheet" type="text/css" href="css/formato.css">
+</head>
+<body>
+	<div id="geral">
+       <div id="topo">
+       <?php include "topo.php";?>
+    </div>
+    <div id="menu">
+    	<?php include "menu.php";?>
+    </div>
+    <div id="conteudo">
+    	<?php include "conteudo_postagem.php";?>
+    </div>
+    <div id="rodape">
+    	<?php include "rodape.php";?>
+     </div>
+  </div><!--fim da div geral-->
+</body>
+</html>
